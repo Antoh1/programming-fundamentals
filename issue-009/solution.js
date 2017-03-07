c2a4c8639b2e3606d71c4d23651764469331784f
Antoh1 <antotiro@gmail.com>
2017-03-07 19:24:00 +0300

    added Solution: Array Reduce function

--- issue-009/solution.js
+++ issue-009/solution.js
@@ -1,4 +1,11 @@
-// implement your solution here
 function sumEven(a) {
-
+	//variable to hold sum of the even numbers
+	evenSum = a.reduce(function(sum, num){
+		//checks for even number in the array
+		if(num%2==0){
+			sum+=num; //updates the sum of even numbers
+		}
+	return sum;	//return value by the reduce function
+	},0);
+    return evenSum; //return value by sumEven function
 }
