997b203d5d8f6ce418e66890137ffc42d584b308
Antoh1 <antotiro@gmail.com>
2017-03-20 16:00:04 +0300

    add Solution: Object Values function

--- issue-022/solution.js
+++ issue-022/solution.js
@@ -1,4 +1,10 @@
-// implement your solution here
 function objectValues(a) {
-
-}
+     
+     var result = []; //output holder
+     var sortedKeys = Object.keys(a).sort(); //sorting the object keys in accending order
+     sortedKeys.forEach(function(key) {
+     	//updating the output holder
+         result.push(a[key]);
+     });
+     return result;
+ } 
