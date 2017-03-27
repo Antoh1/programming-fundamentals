67123a53e13da7e159c43836131dc515267330ac
Antoh1 <antotiro@gmail.com>
2017-03-27 12:09:31 +0300

    added Solution: Person Constructor

--- issue-023/solution.js
+++ issue-023/solution.js
@@ -1,4 +1,13 @@
-// implement your solution here
-function Person(name, age) {
+
+function Person(name, age) { //constructor function to instantiate person objects
+	this.name = name;
+	this.age = age;
+	this.walked = 0;
 
 }
+
+Person.prototype.greet = function(p2){return "hello " + p2.name;} //function greet inherited by Person object
+
+Person.prototype.walk = function(d){this.walked = this.walked + d; //function walk inherited by Person object
+                                    return this.walked;
+                                   }
